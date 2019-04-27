2ac8f07ade0ab72e016f80ff9672c9a1cbe9e0e5
wa1tnr <wa1tnr@users.noreply.github.com>
2019-04-27 18:11:04 +0000

    On branch test-aa Finishing touches.  Should be working program, complete.
    UNTESTED.
    	modified:   gemma_m0_simple-aa.ino

--- Arduino.d/gemma_m0_simple-aa/gemma_m0_simple-aa.ino
+++ Arduino.d/gemma_m0_simple-aa/gemma_m0_simple-aa.ino
@@ -1,8 +1,15 @@
-/* 27 April 18:04 UTC */
+/* Sat Apr 27 18:10:05 UTC 2019 */
+
+/* demo of setup() and loop() for Arduino */
+
+/* reference target: Adafruit Gemma M0 */
+
 
 /* gpio setup/initialization */
 
 #define LED 13
+#define BLINK_TIME_MS 800
+#define BLINK_OFF_TIME_MS 2200
 
 void gpio_init() {
     pinMode(LED, OUTPUT);
@@ -16,11 +23,15 @@ void setup() {
     Serial.println("Simple program begins now.");
     // setup D13 for OUTPUT
     gpio_init();
-
 }
 
 /* LED blink */
-void blink() { } // empty: top down, bottom up, middle out programming style. ;)
+void blink() { // empty: top down, bottom up, middle out programming style. ;)
+    digitalWrite(LED, 1);
+    delay(BLINK_TIME_MS);
+    digitalWrite(LED, 0);
+    delay(BLINK_OFF_TIME_MS);
+}
 
 void loop() {  // runs forever
     for (volatile int i = 987654; i > 0; i--) {
