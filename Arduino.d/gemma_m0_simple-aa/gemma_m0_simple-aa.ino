80f231a87e1c2ea82f5a4201df65e878f0e13037
wa1tnr <wa1tnr@users.noreply.github.com>
2019-04-27 17:47:25 +0000

    On branch test-aa LED blink primitives added. 	modified:   gemma_m0_simple-aa.ino

--- Arduino.d/gemma_m0_simple-aa/gemma_m0_simple-aa.ino
+++ Arduino.d/gemma_m0_simple-aa/gemma_m0_simple-aa.ino
@@ -1,12 +1,18 @@
+/* 27 April 17:41 UTC */
+
 /* Arduino main setup and loop */
 void setup() {
     Serial.begin(38400);
     Serial.println("Simple program begins now.");
+    // setup D13 for OUTPUT
 }
+
+void blink() { } // empty: top down, bottom up, middle out programming style. ;)
 
 void loop() {  // runs forever
     for (volatile int i = 987654; i > 0; i--) {
         // no operation here - time-waster for a delay
     }
     Serial.println("looping ...");
+    blink(); // empty
 }
