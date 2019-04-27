da5295b7e944f358c6f8aea335e5e535ea7512c9
wa1tnr <wa1tnr@users.noreply.github.com>
2019-04-27 17:55:16 +0000

    On branch test-aa gpio setup added (skeletal). 	modified:   gemma_m0_simple-aa.ino

--- Arduino.d/gemma_m0_simple-aa/gemma_m0_simple-aa.ino
+++ Arduino.d/gemma_m0_simple-aa/gemma_m0_simple-aa.ino
@@ -1,12 +1,21 @@
 /* 27 April 17:41 UTC */
 
+/* gpio setup/initialization */
+
+void gpio_init() {
+}
+
 /* Arduino main setup and loop */
+
 void setup() {
     Serial.begin(38400);
     Serial.println("Simple program begins now.");
     // setup D13 for OUTPUT
+    gpio_init();
+
 }
 
+/* LED blink */
 void blink() { } // empty: top down, bottom up, middle out programming style. ;)
 
 void loop() {  // runs forever
